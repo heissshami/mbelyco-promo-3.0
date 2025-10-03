0f17e5dc14bd8a5c83688cd82acb79375358782d
Shami SAMI <140964860+heisssami@users.noreply.github.com>
2025-10-03 02:37:27 +0200

    MBELYCO Promo 3.0 (Initialization).

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -6,9 +6,7 @@ import {
   BadgeCheck,
   Bell,
   ChevronsUpDown,
-  CreditCard,
   LogOut,
-  Sparkles,
 } from "lucide-react"
 
 import {
@@ -99,19 +97,8 @@ export function NavUser({
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
               <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
                 <BadgeCheck />
                 Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCard />
-                Billing
               </DropdownMenuItem>
               <DropdownMenuItem>
                 <Bell />
